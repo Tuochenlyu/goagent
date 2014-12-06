5eaa2e8d905cd18a556eab6c708fa8bb56babab2
tuochenlyu <tuochenlyu@gmail.com>
2014-12-07 02:46:30 +0800

    initial commit

---
+++ scripts.sh
@@ -0,0 +1,6 @@
+git clone git@github.com:Tuochenlyu/goagent.git
+git remote add goagent git@github.com:goagent/goagent.git
+git fetch --all
+#(create branches manually)
+git push -f origin goagent/3.0:3.0
+git push -f origin goagent/wiki:wiki
